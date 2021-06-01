227259b9661e58f06815e140a625aa957914c4a3
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:51:52 +0700

    Initial commit

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+function kalimat() {
+    var a = "No"
+    var b = " Rumah Saya "
+    var c = "30 "
+    var d = "A"
+
+    result = String(a + b + c + d)
+    console.log(result)
+    console.log(result)
+}
+
+kalimat()
